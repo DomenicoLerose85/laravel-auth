9bbff1e55e1ff21699fdf786bc842ff916ce438b
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-22 19:50:15 +0200

    migration_launched

--- database/migrations/2022_06_22_173629_create_posts_table.php
+++ database/migrations/2022_06_22_173629_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 120)->unique();
+            $table->text('content');
+            $table->boolean('published')->default(false);
             $table->timestamps();
         });
     }
